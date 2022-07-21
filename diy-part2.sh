de3d4db23c4e7df5e312fa094c34e572a19119b6
LJHR1999 <107289268+LJHR1999@users.noreply.github.com>
2022-07-21 15:55:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+./scripts/feeds update helloworld
+./scripts/feeds install -a -f -p helloworld
